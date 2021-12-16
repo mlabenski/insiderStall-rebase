48e8a5d826f53c65466782f8f2ddbf30eee9f3f2
Mitchell <mlabenski123@gmail.com>
2021-12-16 03:48:18 -0500

    time stamp

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -12,5 +12,6 @@ firebase.initializeApp({
     appId: process.env.VUE_APP_APP_ID
 });
 
+export const currentTime = firebase.firestore.Timestamp.fromDate(new Date());
 export const db = firebase.firestore()
 export const auth = firebase.auth()
